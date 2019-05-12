d73501d1cab43f2131116b7c71efc0a6246ebff0
Kedar Gupta <guptak@ucla.edu>
2019-05-11 22:00:17 -0700

    add background js for functionality when html5 pushstate is used

--- js/background.js
+++ js/background.js
@@ -1,5 +1,3 @@
-/*chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(null,{file:"displayMatch.js"});
-});*/
-
-console.log("done");
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    chrome.tabs.executeScript(null,{file:"js/displayMatch.js"});
+});
