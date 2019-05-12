611710af5ee8953e5c2f7bef5b85597c78d52d55
Kedar Gupta <guptak@ucla.edu>
2019-05-12 01:28:24 -0700

    picture shows now

--- js/displayMatch.js
+++ js/displayMatch.js
@@ -7,14 +7,22 @@ if(currentURL.includes("watch")){
     d1.insertAdjacentHTML('beforebegin', '<div id="agora" class="flex-container" style=\"width:' + d1.offsetWidth + 'px; height:102px;"></div>');
     
     var d2 = document.getElementById('agora');
-    d2.insertAdjacentHTML('afterbegin', '<span id="pictureSpan">One</span> <span id="titleSpan">Two</span> <span id="pSpan>Three</span>');
+    d2.insertAdjacentHTML('afterbegin', '<span id="pictureSpan"></span> <span id="titleSpan" style="font-size: 12px">Jorge S. Fuentes</span> <span id="pSpan">viewed this video 12 minutes ago.</span>');
     //document.getElementById('related').prepend("<div id=\"agora\">Your friend Jorge Fuentes also watched this video.</div>");
     
-    var d3 = document.getElementById('pictureDiv');
-    d3.insertAdjacentHTML('afterbegin', '<img src="' + chrome.runtime.getURL('img/jorge.png') + '" alt="Your Friend" height="60" width="60">');
+    var d3 = document.getElementById('pictureSpan');
+    d3.insertAdjacentHTML('afterbegin', '<img src="' + chrome.runtime.getURL('img/jorge.png') + '" alt="Your Friend" style="margin: 10px 15px" height="60" width="60">');
     /*var mainDiv = document.createElement("div");
     mainDiv.id ="agoraDiv";
     d.innerHtml = html;
     t1.appendChild(d);*/
     
 }
+
+
+/*
+
+when did they last view it?
+what if multiple people view it?
+
+*/
