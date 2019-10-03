973ad01ced80415ac2acea691c28aa6981eb25cb
Dmitri Brereton <mitrikyle@gmail.com>
2019-10-02 20:57:04 -0700

    wowowoowowowwowowowowowow nice memee

--- js/content.js
+++ js/content.js
@@ -1,32 +1,21 @@
-//THIS IS A WAY TO INJECT SCRIPTS DONT RECOMMEND SORRY
-// var imported = document.createElement('script');
-// imported.src = 'handlebars.js';
-// document.head.appendChild(imported);
+reloadContentScript()
 
-// function generateFriendHTML(friend) {
-//   let handleInfo =
-//   '<div class="card">' + 
-//     '<img src=" {{imgsrc}} " alt="Your Friend" class="ballpic" height="60" width="60">' +
-//     '<span id="titleSpan"> {{name}} </span>' + 
-//   '</div>';
-//    // + '<span id="pSpan">viewed this video {{time}} ago.</span>';
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === "urlChanged") {
+      reloadContentScript()
+    }
+  });
 
-//   let template = Handlebars.compile(handleInfo);
 
-//   let friendHTML = template ({
-//       imgsrc: friend.picUrl,
-//       name: friend.name,
-//       time: friend.timestamp
-//   });
-//   console.log(friendHTML);
-//   return friendHTML;
-// }
 
-const currentURL = location.href;
-console.log("Establishing Signal");
-
-if(currentURL.includes("facebook.com")) {
-    require("./facebook/facebook.js");
-} else {
-    require("./test.js");
+function reloadContentScript(){
+    console.log("Hello, I am being caallled my boy")
+    const currentURL = location.href;
+    console.log("Establishing Signal");
+    
+    if(currentURL.includes("facebook.com")) {
+        require("./facebook/facebook.js");
+    } else {
+        require("./test.js");
+    }
 }
