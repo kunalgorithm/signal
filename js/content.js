26286b9443bbc48066e7b11e4444a7e021cc377e
Jorge Fuentes <jsjfuentesj@gmail.com>
2019-10-02 17:58:47 -0700

    Chrome Extension Base

---
+++ js/content.js
@@ -0,0 +1,26 @@
+//THIS IS A WAY TO INJECT SCRIPTS DONT RECOMMEND SORRY
+// var imported = document.createElement('script');
+// imported.src = 'handlebars.js';
+// document.head.appendChild(imported);
+
+// function generateFriendHTML(friend) {
+//   let handleInfo =
+//   '<div class="card">' + 
+//     '<img src=" {{imgsrc}} " alt="Your Friend" class="ballpic" height="60" width="60">' +
+//     '<span id="titleSpan"> {{name}} </span>' + 
+//   '</div>';
+//    // + '<span id="pSpan">viewed this video {{time}} ago.</span>';
+
+//   let template = Handlebars.compile(handleInfo);
+
+//   let friendHTML = template ({
+//       imgsrc: friend.picUrl,
+//       name: friend.name,
+//       time: friend.timestamp
+//   });
+//   console.log(friendHTML);
+//   return friendHTML;
+// }
+
+const currentURL = location.href;
+console.log("CONTENT SCRIPT HELLO", currentURL);
