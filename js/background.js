401313eddb5288ab56ab342abc329a25d10adf08
Dmitri Brereton <mitrikyle@gmail.com>
2019-10-04 10:43:25 -0700

    smh

--- js/background.js
+++ js/background.js
@@ -1,4 +1,30 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(urlChanged);
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "twitter.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "facebook.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "linkedin.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "youtube.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "reddit.com" }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
 
 function urlChanged() {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
