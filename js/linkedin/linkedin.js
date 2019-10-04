4f9af096c2931f0f185bd2cc321686ba6f95a121
Jorge Fuentes <jsjfuentesj@gmail.com>
2019-10-03 17:17:30 -0700

    Linkedin Hidden Bb

--- js/linkedin/linkedin.js
+++ js/linkedin/linkedin.js
@@ -1,59 +1,80 @@
 module.exports = { main };
 
+let elementsHidden = [];
+
 function hideEl(el) {
+  //Instead of visibilty just move off page because weird to deal with svgs
   // https://github.com/highcharts/highcharts/issues/2597
-  // SVGS are annoying to deal with, they take some time to load and override the visilibity setting in linkedin
+  // Actually store prior style in DOM
+  el.signal_prior_style = {position: el.style.position, right: el.style.right};
   el.style.position = "relative";
-  el.style.top = "-999em";
+  el.style.right = "2000em";
+  elementsHidden.push(el);
+  console.log(elementsHidden);
+}
+
+function showLinkedin() {
+  elementsHidden.map(e => {
+    console.log("show", e);
+    e.style.position = e.signal_prior_style.position;
+    e.style.right = e.signal_prior_style.right;
+    e.signal_prior_style = undefined;
+  });
+  elementsHidden = [];
 }
 
 function main(hide) {
   console.log("🤦‍ running linkedin script");
 
   if(hide) {
-    //remove messaging bar
-    function removeMessagingBar() {
-      const myInterval = setInterval(() => {
-        const msgBar = document.querySelector('#msg-overlay');
-        if(msgBar === null) {
-          return;
-        } else {
-          hideEl(msgBar);
-          clearInterval(myInterval);
-        }
-      }, 333);
-    }
-  
-    const currentURL = location.href;
-    if(currentURL.includes("linkedin.com/feed/")) {
-      
-    }
-  
-    removeMessagingBar();
-  
-    function removeFeed() {
-      const feedEl = document.querySelector('.feed-shared-update-v2');
-      if(feedEl !== null) {
-        const feedContainer = feedEl.parentElement.parentElement;
-        hideEl(feedContainer);
-        // const feedDropDown = feedContainer.previousSibling();
-        // feedDropDown.style.visibility = "hidden";
+    hideLinkedin();
+  } else {
+    showLinkedin();
+  }
+}
+
+function hideLinkedin() {
+  //remove messaging bar
+  function removeMessagingBar() {
+    const myInterval = setInterval(() => {
+      const msgBar = document.querySelector('#msg-overlay');
+      if(msgBar === null) {
+        return;
+      } else {
+        hideEl(msgBar);
+        clearInterval(myInterval);
       }
-    }
-  
-    removeFeed();
-    //remove feed
-  
-    //remove sidebar
-    function removeSideBar() {
-        const sidebar = document.querySelector('.right-rail');
-        if(sidebar !== null) {
-          hideEl(sidebar);
-        }
-        //sidebar should be available at start if its there
-    }
-  
-    removeSideBar();
+    }, 333);
   }
 
+  const currentURL = location.href;
+  if(currentURL.includes("linkedin.com/feed/")) {
+    
+  }
+
+  removeMessagingBar();
+
+  function removeFeed() {
+    const feedEl = document.querySelector('.feed-shared-update-v2');
+    if(feedEl !== null) {
+      const feedContainer = feedEl.parentElement.parentElement;
+      hideEl(feedContainer);
+      // const feedDropDown = feedContainer.previousSibling();
+      // feedDropDown.style.visibility = "hidden";
+    }
+  }
+
+  removeFeed();
+  //remove feed
+
+  //remove sidebar
+  function removeSideBar() {
+      const sidebar = document.querySelector('.right-rail');
+      if(sidebar !== null) {
+        hideEl(sidebar);
+      }
+      //sidebar should be available at start if its there
+  }
+
+  removeSideBar();
 }
