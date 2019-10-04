2e323fe077707885de3141cc1972834f806ad7cf
Kunal <me@kunal.sh>
2019-10-04 10:37:48 -0700

    📺youtube script

--- js/youtube/youtube.js
+++ js/youtube/youtube.js
@@ -1,9 +1,11 @@
 module.exports = { main };
 
 function main(hide) {
-  console.log("🏃‍♂ running youtube.js");
+  console.log("🏃‍♂ running youtube.js", hide);
 
-  document.getElementById("page-manager").style.visibility = "hidden";
+  document.getElementById("related").style.visibility = "hidden";
+  document.getElementById("comments").style.visibility = "hidden";
+  // document.getElementById("page-manager").style.visibility = "hidden";
   // document
   //   .getElementsByClassName("mbl")[0]
   //   .parentNode.removeChild(document.getElementsByClassName("mbl")[0]);
