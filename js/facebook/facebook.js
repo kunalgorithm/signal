4fb5ad9ba4068e057b4f78dd4053a02dd35e137a
suyashSax <suyash2099@gmail.com>
2019-10-03 20:13:06 -0700

    paginate infinite scroll

--- js/facebook/facebook.js
+++ js/facebook/facebook.js
@@ -1,9 +1,67 @@
 module.exports = { main };
 
+console.log("🤦‍ running facebook script");
+
+let child 
+let parent
+let counter
+
 function main(hide) {
-  console.log("🤦‍ running facebook script");
-  document.getElementById("left_nav_section_nodes").style.visibility = "hidden";
-  document
-    .getElementsByClassName("mbl")[0]
-    .parentNode.removeChild(document.getElementsByClassName("mbl")[0]);
+  if (counter === undefined) counter = 0 
+  else counter++
+
+  if (counter == 0) {
+    console.log("Screw the first time")
+    return
+  }
+    readNextPage()
+    removeNextPage()
+}
+
+let newsFeedDom = document.getElementById("content_container");
+  var button = document.createElement("button");
+  var text = document.createTextNode("LOAD MORE");
+  button.appendChild(text)
+
+  button.addEventListener("click",  () => {
+    showNextPage()
+    setTimeout( function () {
+      readNextPage()
+      removeNextPage()
+    }, 5000)
+  })
+  newsFeedDom.appendChild(button);
+
+function removeNextPage() {
+  console.log("Remove Next Page", child, parent)
+  if (!document.getElementsByClassName("mbl")[0]) {
+    console.log("Null to remove")
+    return
+  }
+  document.getElementsByClassName("mbl")[0].parentNode.removeChild(document.getElementsByClassName("mbl")[0]);
+}
+
+function readNextPage() {
+  console.log("Read Next Page", child, parent)
+  if (document.getElementsByClassName("mbl")[0] == undefined) {
+    console.log("Attempt to read next page FAILED")
+    return
+  }
+  child = document.getElementsByClassName("mbl")[0]
+  parent = document.getElementsByClassName("mbl")[0].parentNode
+}
+
+function showNextPage() {
+  console.log("Show Next Page", child, parent)
+  if (child === undefined) {
+    console.log("No child")
+    return
+  }
+
+  if (parent === undefined) {
+    console.log("No parent")
+    return
+  }
+
+  parent.appendChild(child)
 }
